37dc134eb1b3814dfaa853cd65840ca41f153f7b
guguiobri54 <guiguiobri54@gmail.com>
2019-02-18 20:30:02 +0100

    d

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Http\Controllers;
-
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
-
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 
 class HomeController extends Controller
@@ -13,20 +12,21 @@ class HomeController extends Controller
      *
      * @return void
      */
+    protected $LoginController;
+
+
     public function __construct()
     {
         $this->middleware('auth');
     }
-
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(LoginController $loginController)
     {
-
-
-            return view('/home');
+        $redirection = $this->LoginController->authenticated;
+        redirect ($redirection);
     }
 }
